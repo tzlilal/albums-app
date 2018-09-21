1dd0bb03d9f7d70db9aed6a6f8785ba760535416
tzlil <tzlil330@gmail.com>
2018-09-21 18:20:53 +0300

    add style to CardSections and add data from album API

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,65 @@
 import React from 'react'; 
-import {View, Text} from 'react-native'; 
+import {View, Text, Image} from 'react-native'; 
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image } = album; 
+    const { 
+        headerContentStyle, 
+        thumbnailStyle, 
+        thumbnailContainerStyle, 
+        headerTextStyle, 
+        imageStyle 
+    } = styles; 
+
     return (
         <Card>
-            <Text>{props.album.title}</Text>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image 
+                        style={thumbnailStyle}
+                        source={{uri: thumbnail_image}} 
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image 
+                    style={imageStyle}
+                    source={{uri: image}} 
+                /> 
+            </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column', 
+        justifyContent: 'space-around'
+    }, 
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50, 
+        width: 50, 
+    }, 
+    thumbnailContainerStyle: {
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        marginRight: 10, 
+        marginLeft: 10
+    }, 
+    imageStyle: { 
+        height: 300,
+        flex: 1, 
+        width: null
+    }
 };
 
 export default AlbumDetail;
--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,11 +1,24 @@
 import React from 'react'; 
 import {View} from 'react-native'; 
 
-const CardSection = () => { 
+const CardSection = (props) => { 
     return (
-        <View> 
+        <View style={styles.containerStyle}>
+            {props.children} 
         </View> 
     ); 
 }; 
+
+const styles = {
+    containerStyle: {
+        borderBottomWidth: 1, 
+        padding: 5, 
+        backgroundColor: '#fff', 
+        justifyContent: 'flex-start', 
+        flexDirection: 'row', 
+        borderColor: '#ddd', 
+        position: 'relative'
+    }
+};
 
 export default CardSection; 
